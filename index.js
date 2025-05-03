94067f8cddc8844eb91e8db00145c284661ef09b
Pawan Sankalpa <Pawansankalpanew123@gmail.com>
2025-05-03 15:20:08 +0530

    Practice routes

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hey It's Pawan")
+    res.send("Hey It's Pawan from the Home Page")
+})
+
+app.get("/about", (req, res) => {
+    res.send("Hey It's Pawan from the about page")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Hey It's Pawan from the contact page")
 })
 
 app.listen(port, () => {
