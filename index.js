37900e9558f7fb4670e5bcbd6e9ddd5dc951de7a
Pawan Sankalpa <Pawansankalpanew123@gmail.com>
2025-05-04 10:39:29 +0530

    Practice express, add css

--- index.js
+++ index.js
@@ -1,52 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import morgan from "morgan";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const { urlencoded } = bodyParser;
 
 const app = express();
 const port = 3000;
 
-app.use(morgan("tiny"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny")); // helpful assistant that tells you everything your server is doing.
+app.use(bodyParser.urlencoded({ extended: true }));// Parses form data from the request.
 
-//create my own middleware
-function logTime(req, res, next){
-  console.log(`Time: ${new Date().toLocaleDateString()}`);
-  next();
-}
-
-app.use(logTime)
-
-// create a middleware with conditions
-function guard(req, res, next){
-  const secretKey = req.query.secrets;
-  if (secretKey === "studpidDonkey"){
-    next(); // Let him in
-  } else {
-    res.send("<h>Access denied</h1>");
-  }
-}
-
-app.get("/protected", guard, (req, res) => {
-  res.send("You are inside the secret page of the Stuupid Donkey.")
-});
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
-
-// app.post("/submit", (req, res) => {
-//   console.log(req.body);
-// });
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
-  res.send("Hey It's Pawan from the Home Page");
+  res.sendFile(__dirname + "/public/HomePage.html");// serve the homePage
 });
 
+app.get("/form", (req, res) => {
+  res.sendFile(__dirname + "/public/signUpForm.html");// serve the form
+})
+
+app.post("/submit", (req, res) => {
+  {
+    const { fName, lName } = req.body;
+    res.send(`<h1>Hey, are you ${fName} ${lName}</h2>`)
+  }
+});
 
 app.listen(port, () => {
-  console.log(`We are running on the port: ${port}`);
+  console.log(`Listening on port ${port}`);
 });
