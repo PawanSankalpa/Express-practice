4717dd066b4d8d71c546645650e02efe6bd4a2d3
Pawan Sankalpa <Pawansankalpanew123@gmail.com>
2025-05-04 18:03:18 +0530

    Create logIn middleware, Css styling

--- index.js
+++ index.js
@@ -9,22 +9,46 @@ const app = express();
 const port = 3000;
 
 app.use(morgan("tiny")); // helpful assistant that tells you everything your server is doing.
-app.use(bodyParser.urlencoded({ extended: true }));// Parses form data from the request.
+app.use(bodyParser.urlencoded({ extended: true })); // Parses form data from the request.
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
+
+// for monitoring ( to debugging, security, usage analysis, auditing)
+app.use((req, res, next) => {
+  const time = new Date().toISOString();
+  const ip = req.id;
+  const userAgent = req.get("User-Agent"); //Which browser, Which operating system, sometimes even the device type.
+  const path = req.originalUrl;
+  console.log(`[${time}] ${ip} ${userAgent} accessed ${path}`);
+  next();
+});
+//-----------------------------------------------------------------------//
+
+// self created middleware logIn- to check if the client is the admin.
+function logIn(req, res, next) {
+  const { username, password } = req.body;
+  const adminDetails = { username: "Pawan", password: "stupidDonkey" };
+
+  // coding step by step is to be clear. otherwise can use ternary operators
+  if (username === adminDetails.username && password === adminDetails.password) {
+    next(); //let the admin in
+  } else {
+    res.sendFile(__dirname + "/public/userPage.html"); //serve the userPage
+  }
+}
+//----------------------------------------------------------------------//
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/HomePage.html");// serve the homePage
+  res.sendFile(__dirname + "/public/HomePage.html"); // serve the homePage
 });
 
 app.get("/form", (req, res) => {
-  res.sendFile(__dirname + "/public/signUpForm.html");// serve the form
-})
+  res.sendFile(__dirname + "/public/signUpForm.html"); // serve the Sign in form
+});
 
-app.post("/submit", (req, res) => {
+app.post("/admin", logIn, (req, res) => {
   {
-    const { fName, lName } = req.body;
-    res.send(`<h1>Hey, are you ${fName} ${lName}</h2>`)
+    res.sendFile(__dirname + "/public/adminPage.html"); // serve the adminPage
   }
 });
 
